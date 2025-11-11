08af2bb330a68d4cb4d9478ede84df666df67f88
tangjianming <tangjianming@bytedance.com>
2025-11-11 10:47:38 +0800

    修复PS5编译脚本

--- make_ps.bat
+++ make_ps.bat
@@ -5,18 +5,12 @@ cd %CUR_DIR%
 del /s/q buildPS5
 mkdir buildPS5 & pushd buildPS5
 
-cmake -DCMAKE_C_COMPILER="%SCE_PROSPERO_SDK_DIR%/host_tools/bin/prospero-clang.exe" ^
-	-DCMAKE_CXX_COMPILER="%SCE_PROSPERO_SDK_DIR%/host_tools/bin/prospero-clang.exe" ^
-	-DCMAKE_AS="%SCE_PROSPERO_SDK_DIR%/host_tools/bin/prospero-clang.exe" ^
-	-DCMAKE_AR="%SCE_PROSPERO_SDK_DIR%/host_tools/bin/prospero-llvm-ar.exe" ^
-	-DCMAKE_LINKER="%SCE_PROSPERO_SDK_DIR%/host_tools/bin/prospero-lld.exe" ^
-	-DCMAKE_NM="%SCE_PROSPERO_SDK_DIR%/host_tools/bin/prospero-llvm-nm.exe" ^
-	-DCMAKE_OBJCOPY="%SCE_PROSPERO_SDK_DIR%/host_tools/bin/prospero-llvm-objcopy.exe" ^
-	-DCMAKE_OBJDUMP="%SCE_PROSPERO_SDK_DIR%/host_tools/bin/prospero-llvm-objdump.exe" ^
-    -G "Unix Makefiles" ^
+cmake -DCMAKE_TOOLCHAIN_FILE="C:/Program Files (x86)/SCE/Prospero/Tools/CMake/PS5.cmake" ^
+    -G "Visual Studio 17 2022" ^
     -DCMAKE_SYSTEM_NAME=Playstation ^
     -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON ^
     -DPS_PLATFORM=1 ^
+	-DCMAKE_LINKER="C:/Program Files (x86)/SCE/Prospero SDKs/11.000/host_tools/bin/prospero-lld.exe" ^
     ..
     
 popd
